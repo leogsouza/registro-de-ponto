7a48f2aace54f41578605865d12fd10748dc040a
Leonardo Souza <leogsouza@gmail.com>
2017-02-23 16:37:58 -0300

    Add function to prepare entries to show on view

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response;
@@ -10,13 +12,32 @@ const checkStatus = (response) => {
 
 const parseJSON = (response) => response.json();
 
+const prepareEntries = (entries) => {
+  return entries.map( (entry) => {
+    const date = moment(entry.time_entry).format("DD/MM/YYYY");
+    const time = moment(entry.time_entry).format("HH:mm");
+
+    return {date: date, time: time};
+  }).reduce((acum, item) => {
+    const key = item.date;
+
+    acum[key] = acum[key] || [];
+    acum[key].push(item.time);
+
+    return acum;
+  }, {});
+
+
+}
+
 
 const getEntries = () =>
   fetch('/entries?_sort=time_entry', {
     accept: 'application/json',
     mode: 'cors'
   }).then(checkStatus)
-  .then((parseJSON));
+  .then((parseJSON))
+  .then(prepareEntries);
 
 const Client = {getEntries};
 
--- client/src/components/EntryList.js
+++ client/src/components/EntryList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Client from './Client';
-import moment from 'moment';
+
 class EntryList extends React.Component {
 
   constructor(props) {
@@ -13,17 +13,16 @@ class EntryList extends React.Component {
 
   componentDidMount() {
     Client.getEntries().then((response) => {
+
       this.setState({entries: response});
     })
   }
 
-
-
   render() {
     return (
       <div>
-        {this.state.entries.map(entry =>
-          <p>{entry.time_entry}</p>)}
+        {JSON.stringify(this.state.entries)/*(entry =>
+          <p>{JSON.stringify(entry)}</p>)*/}
       </div>
     );
   }
